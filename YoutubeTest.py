55685d3438113e4f7a20cb8f75f2f50aa056d23b
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-01 11:35:28 +0000

    Update YoutubeTest.py

--- YoutubeTest.py
+++ YoutubeTest.py
@@ -9,8 +9,9 @@ search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().de
 url = "http://www.youtube.com/watch?v=" + search_results[0]
 
 video = pafy.new(url)
-bestaudio = video.getbestaudio()
-downloadChoice = input("Would you like to download" + video.title + "? (Y/n) ")
+bestaudio = video.getbestaudio(preftype="m4a")
+
+downloadChoice = input("Would you like to download: " + video.title + "? (Y/n) ")
 if downloadChoice == 'Y':
     bestaudio.download()
 finalChoice = input("Is that all? (Y/n) ")
