a3f68fe59631042d8d5d9308cfc22eccee451104
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-06 14:43:32 +0000

    Create MusicPlayerTest.py
    Music player that plays local files. Allows you to input a search and finds it. READ - only works if you type the exact name of file without the .mp3 extension, only works for .mp3, currently only works on mac because of file directory, will play the whole song but the program will not end until you type STOP, doesn't print message when file not found. Currently working on it to see if it something we can actually use in the chatbot.

---
+++ MusicPlayerTest.py
@@ -0,0 +1,27 @@
+import os
+from os.path import join
+import vlc
+import time
+
+lookfor= input("Search for... ")
+search = lookfor + str('.mp3')
+print("searching...")
+for root, dirs, files in os.walk('/Users'):
+    root
+    if search in files:
+        if True:
+            file = join(root, search)
+            print(file)
+            choicePlay = input("Would you like to play this file? (Y/n) ")
+            if choicePlay == 'Y':
+                song = vlc.MediaPlayer(file)
+                song.play()
+                user_stop = input("Type STOP to stop playing... ")
+                if user_stop == "STOP":
+                    song.stop()
+                    exit()
+                else:
+                    time.sleep(20)
+        elif False:
+            print("Sorry I couldn't find the file :(")
+            break
