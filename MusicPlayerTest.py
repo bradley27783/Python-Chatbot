26d0bb69d9e37047ef6ce3c2afae6ddd30dd3778
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-10 13:24:10 +0000

    Update MusicPlayerTest.py

--- MusicPlayerTest.py
+++ MusicPlayerTest.py
@@ -1,27 +1,30 @@
-import os
-from os.path import join
-import vlc
-import time
+def musicPlayer():
 
-lookfor= input("Search for... ")
-search = lookfor + str('.mp3')
-print("searching...")
-for root, dirs, files in os.walk('/Users'):
-    root
-    if search in files:
-        if True:
-            file = join(root, search)
-            print(file)
-            choicePlay = input("Would you like to play this file? (Y/n) ")
-            if choicePlay == 'Y':
-                song = vlc.MediaPlayer(file)
-                song.play()
-                user_stop = input("Type STOP to stop playing... ")
-                if user_stop == "STOP":
-                    song.stop()
+    import os
+    from os.path import join
+    import vlc
+    import time
+
+    lookfor= input("Search for... ")
+    search = lookfor + str('.mp3')
+    print("searching...")
+    for root, dirs, files in os.walk('/Users'):
+        root
+        if search in files:
+            if True:
+                file = join(root, search)
+                print(file)
+                choicePlay = input("Would you like to play this file? (Y/n) ")
+                if choicePlay == 'Y':
+                    song = vlc.MediaPlayer(file)
+                    song.play()
+                if choicePlay == 'n':
                     exit()
-                else:
-                    time.sleep(20)
-        elif False:
-            print("Sorry I couldn't find the file :(")
-            break
+                    user_stop = input("Type STOP to stop playing... ")
+                    if user_stop == "STOP":
+                        song.stop()
+                        exit()
+            else:
+                print("Sorry I couldn't find the file :(")
+                break
+musicPlayer()
