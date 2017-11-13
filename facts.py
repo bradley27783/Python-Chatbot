dfb441a0741298a8f45eed2bb21b35744b489347
kyle <gillett5@uni.coventry.ac.uk>
2017-11-13 10:39:02 +0000

    updated fact lists

--- facts.py
+++ facts.py
@@ -1,3 +1,18 @@
 def facts():
-    if artist==kanye
-        print("kanye is a rapper")
+    artist = str(input("Please enter a artist: "))
+    if artist=="kanye":
+        print("Did you know, Kanye Omari West was born June 8, 1977 in Atlanta?")
+        print("Kanye attended Chicago State University/Columbia College in Chicago. He dropped out to pursue music which is why he named his 2004 debut album, \"The College Dropout.\"")
+        print("Kanye was named International Man of the Year by GQ in 2007 at a ceremony at Covent Garden's Opera House in London")
+        print("Kanye was in a near fatal car accident while he was driving home from the studio in October 2002. The injuries were bad and he had to have a metal plate put into his chin.")
+    if artist=="the beatles":
+        print("Did you know, In America, the beatles had 20 singles that topped the Billboard Hot 100 and 19 albums that made No.1 in the Billboard album charts.")
+        print("The Beatles were first created in Liverpool in 1960")
+        print("Their first hit song was “Love Me Do” in late 1962.")
+        print("The last time that all four Beatles recorded together was 20th of August 1969 where they finished ‘I Want You (She’s So Heavy’).")
+        print("The Beatles have been named, collectively, as among the 20th Century’s most influential people.")
+    if artist=="rick astley":
+        print("Rick left school at the age of 16. He drove a delivery van for his family's business, a garden store.")
+        print("Rick Astley is listed in the Guiness Book of World Records for being the first male solo artist to have his first 8 singles reach the Top 10 in the UK. This feat has never been repeated since.")
+        print("")
+facts()
