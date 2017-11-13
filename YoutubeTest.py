725ec6a488728e40f9f620c495257d38c6b2a30d
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-13 09:08:44 +0000

    Update YoutubeTest.py

--- YoutubeTest.py
+++ YoutubeTest.py
@@ -16,15 +16,15 @@ def getMusic():
         youtube_content = urllib.request.urlopen("http://www.youtube.com/results?" + user_search)     #Searches youtube using user input
         youtube_results = re.findall(r'href=\"\/watch\?v=(.{11})', youtube_content.read().decode())     #Retrieves the search results from youtube and decodes them
         url1 = "http://www.youtube.com/watch?v=" + youtube_results[0]      #Saves the url of the first search result
-        url2 = "http://www.youtube.com/watch?v=" + youtube_results[1]
-        url3 = "http://www.youtube.com/watch?v=" + youtube_results[2]
-        url4 = "http://www.youtube.com/watch?v=" + youtube_results[3]
-        url5 = "http://www.youtube.com/watch?v=" + youtube_results[4]
-        url6 = "http://www.youtube.com/watch?v=" + youtube_results[5]      #Saves the url of the first search result
-        url7 = "http://www.youtube.com/watch?v=" + youtube_results[6]
-        url8 = "http://www.youtube.com/watch?v=" + youtube_results[7]
-        url9 = "http://www.youtube.com/watch?v=" + youtube_results[8]
-        url10 = "http://www.youtube.com/watch?v=" + youtube_results[9]
+        url2 = "http://www.youtube.com/watch?v=" + youtube_results[2]
+        url3 = "http://www.youtube.com/watch?v=" + youtube_results[4]
+        url4 = "http://www.youtube.com/watch?v=" + youtube_results[6]
+        url5 = "http://www.youtube.com/watch?v=" + youtube_results[8]
+        url6 = "http://www.youtube.com/watch?v=" + youtube_results[10]      #Saves the url of the first search result
+        url7 = "http://www.youtube.com/watch?v=" + youtube_results[12]
+        url8 = "http://www.youtube.com/watch?v=" + youtube_results[14]
+        url9 = "http://www.youtube.com/watch?v=" + youtube_results[16]
+        url10 = "http://www.youtube.com/watch?v=" + youtube_results[18]
         video1 = pafy.new(url1)         #Assigns the url to pafy and names it video
         video2 = pafy.new(url2)
         video3 = pafy.new(url3)
@@ -88,6 +88,41 @@ def getMusic():
                         print("10." + video10.title)
                         while True:
                                 choice1 = input("Choose the correct song by typing the correct number: ")
+                                if choice1 == '1':
+                                        print("Opening Browser...")
+                                        time.sleep(1)
+                                        print("Goodbye :)")
+                                        time.sleep(0.5)
+                                        webbrowser.open_new(url1) 
+                                        return()#Opens the url in your default browser
+                                elif choice1 == '2':
+                                        print("Opening Browser...")
+                                        time.sleep(1)
+                                        print("Goodbye :)")
+                                        time.sleep(0.5)
+                                        webbrowser.open_new(url2)
+                                        return()
+                                elif choice1 == '3':
+                                        print("Opening Browser...")
+                                        time.sleep(1)
+                                        print("Goodbye :)")
+                                        time.sleep(0.5)
+                                        webbrowser.open_new(url3)
+                                        return()
+                                elif choice1 == '4':
+                                        print("Opening Browser...")
+                                        time.sleep(1)
+                                        print("Goodbye :)")
+                                        time.sleep(0.5)
+                                        webbrowser.open_new(url4)
+                                        return()
+                                elif choice1 == '5':
+                                        print("Opening Browser...")
+                                        time.sleep(1)
+                                        print("Goodbye :)")
+                                        time.sleep(0.5)
+                                        webbrowser.open_new(url5)
+                                        return()
                                 if choice1 == '6':
                                         print("Opening Browser...")
                                         time.sleep(1)
@@ -128,5 +163,6 @@ def getMusic():
                         
                 else:
                         print("Sorry that was not an option, try again")
+
 
 getMusic()
