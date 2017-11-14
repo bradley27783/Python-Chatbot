f7b798e40e3f70bd380495cf19bbfafc73e0cb1b
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-14 13:51:49 +0000

    Create PersonalisedList.py

---
+++ PersonalisedList.py
@@ -0,0 +1,25 @@
+import os
+from os.path import join
+from collections import Counter
+
+path = os.path.dirname(os.path.realpath(__file__))
+for f_name in os.listdir(path):
+    if f_name.startswith('artistlist.txt'):
+        filename = join(path, f_name)
+        print(filename)
+        
+top10List=filename
+artistList = open(filename, "r")
+artistList = artistList.read()
+artistList = artistList.split(',')
+
+
+
+
+#artist=input("Artist?")#Should be made to take the input(artist)everytime user searches somthing
+#Top10List.append(artist)#Adds to list
+print("Your Top 10 Artists are:")
+
+for count, elem in sorted(((artistList.count(e), e) for e in set(artistList)), reverse=True):
+    listmode = '%s (%d)' % (elem, count) #Sorts a the list Descending
+    print(elem)
