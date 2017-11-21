2ff008824f854033525873cdee7488b7cc55e014
Georgios Economou (economog) <economog@coventry.ac.uk>
2017-11-21 12:40:05 +0000

    Update Top5Artists.py

--- Top5Artists.py
+++ Top5Artists.py
@@ -4,7 +4,7 @@ from collections import Counter
 #lines = file.readlines()
 
 
-#ArtistList=["Rihanna","Drake","Drake","DJ,Blue"]
+ArtistList=["Rihanna","Drake","Drake","DJ","Blue"]
 Artist=input("User searches and we get artist")
 ArtistList.append(Artist)
 
