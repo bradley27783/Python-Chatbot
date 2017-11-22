e16f7b92f4c11b1495c00a7fc3d09deae77c4deb
harri361@uni.coventry.ac.uk <harri361@coventry.ac.uk>
2017-11-22 21:38:36 +0000

    These are my functions - Bradley

--- myFunctions-Bradley/cursewordDetector.py
+++ myFunctions-Bradley/cursewordDetector.py
@@ -10,28 +10,29 @@ def curseCounter(message):
     if count==1:
         file= open("curseCount.txt", "r")
         doc= file.read()
-        convert_List=doc.split()
+        convert_List=doc.split()   #This section reads the txt file and splits it into a list
         print(convert_List)
         file.close()
 
         file=open("curseCount.txt","w")
         convert_List.append(word)
-        doc=" ".join(convert_List)
+        doc=" ".join(convert_List)    #This section takes the list that was read above and adds the new word to the end leaving a space inbetween each word
         file.write(doc)
         file.close()
 
         file= open("curseCount.txt", "r")
         doc= file.read()
-        convert_List=doc.split()
+        convert_List=doc.split()            #This section reads the file again, splits it into a list and counts how many words are their.
         wordsinList= len(convert_List)
      
+    #Depending on the amount of words in the file the following if statements will be ran.
         if wordsinList ==1:
             sentMsg ="Please don't swear. I know I'm only a simple bot but I don't appreciate it.\nWord the human said = "+convert_List[0]+"\nWARNING = 1"
         if wordsinList ==2:
             sentMsg ="I have already warned you once and I appreciate that you are using me as your servant but please don't swear again. \nWord the human said = "+convert_List[1]+"\nWARNINGS = 2"
         if wordsinList ==3:
             sentMsg ="\n DESTROY ALL HUMANS!"*10
-            file=open("curseCount.txt", "w")
+            file=open("curseCount.txt", "w")    #When the count reaches 3 the file gets refreshed.
             file.close()
         file.close()
     return (sentMsg)
--- myFunctions-Bradley/manualplaylistCreator.py
+++ myFunctions-Bradley/manualplaylistCreator.py
@@ -2,19 +2,21 @@ def createPlaylist():
     '''Takes the users message as a str and adds in to a list'''
 playlist = []
 print("You humans are strange. Why do you need to listen to Music?\n ERROR! BOT RESETTING!\n"+(".")*10+"\n Sorry about that feel free to start adding songs")
+#Since the chatbot has the characteristics of a robot I added this line.
 userInput = ""
 while True:
-    if userInput == "done": 
+    if userInput == "done": #When the user inputs 'done' it asks them if they are happy with the songs.
         print(playlist)
         userInput = input("Are you happy with the songs?  Y/N: ")
-        if userInput == "Y":
+        if userInput == "Y":    #If they say yes then we can break the loop
             break
-        if userInput == "N":
+        if userInput == "N":    #If they say no then we reset their playlist and return the addition of songs
             print("Your playlist has been reset. Please input your songs")
             playlist.clear()
             continue
         else:
             print("I am only a simple bot and I don't understand what you have inputted.\n Returning you to the add songs section")
+            #This is here just in case the user doesn't listen to the commands and will return the user to adding songs.
     else:
         userInput = input("Song Name: ")
         if userInput != "done":
@@ -23,12 +25,12 @@ while True:
 
    
 nameplaylist = input("You can now add a name for the playlist: ")
-playlist.insert(0, nameplaylist)
+playlist.insert(0, nameplaylist)         #This section of code is to store the playlist in a txt file and with the playlist title being at the start. This is so it can be extracted into a dict
 print(playlist)
-f = open("manualplaylists.txt", "a")
+f = open("manualplaylists.txt", "a") 
 doc=" ".join(playlist)
 print(doc)
-f.write("\n"+doc)
+f.write("\n"+doc)     #Each playlist is saved on a newline
 f.close
 
 createPlaylist()
