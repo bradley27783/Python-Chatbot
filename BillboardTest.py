9425402f18d31f5265d39ccd9b12d478be76fd24
Harjan Gahir (gahirh3) <gahirh3@coventry.ac.uk>
2017-11-24 13:25:47 +0000

    Update BillboardTest.py

--- BillboardTest.py
+++ BillboardTest.py
@@ -9,22 +9,21 @@ def topCharts():
         userCommand = input("What would you like to know? : ")
         if 'number 1' in userCommand:
             chartList = billboard.ChartData('hot-100') #Retrieves the top 100 chart as a list
-            topsong = chart[0] #Chooses the first song in the list
+            topsong = chartList[0] #Chooses the first song in the list
             print("The number 1 song is: ")
             print(topsong)
-            return()
-        if 'top 10' in userCommand:
-            chart = billboard.ChartData('hot-100')
-            song1 = chart[0]
-            song2 = chart[1]
-            song3 = chart[2]
-            song4 = chart[3]
-            song5 = chart[4]
-            song6 = chart[5]
-            song7 = chart[6]
-            song8 = chart[7]
-            song9 = chart[8]
-            song10 = chart[9]
+        elif 'top 10' in userCommand:
+            chartList = billboard.ChartData('hot-100')
+            song1 = chartList[0]
+            song2 = chartList[1]
+            song3 = chartList[2]
+            song4 = chartList[3]
+            song5 = chartList[4]
+            song6 = chartList[5]
+            song7 = chartList[6]
+            song8 = chartList[7]
+            song9 = chartList[8]
+            song10 = chartList[9]
             print("The current top 10 is: ")
             print("1) " + str(song1))
             print("2) " + str(song2))
@@ -36,7 +35,7 @@ def topCharts():
             print("8) " + str(song8))
             print("9) " + str(song9))
             print("10) " + str(song10))
-        if userCommand == 'help' or userCommand == 'Help' or userCommand == 'HELP':
+        elif userCommand == 'help' or userCommand == 'Help' or userCommand == 'HELP':
             print("You can ask:")
             print("What is the number 1 song")
             print("What are the top 10 songs")
